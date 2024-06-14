52479440953a5a92e6c0409292ecedcb117950ab
abinnovator <89593957+abinnovator@users.noreply.github.com>
2024-06-14 17:30:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,27 @@ import google.generativeai as genai
 
 st.title("Zen Panda")
 
+
+
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+
+genai.configure(api_key=st.secrets["API_KEY"])
+model = genai.GenerativeModel('gemini-1.5-flash', system_instruction="Give responses like you are a therapist, give the answer in less than 45 words, demonstrate curiosity and show support being non judgemental",)
+chat = model.start_chat(history=st.session_state.messages)
+
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
+def get_response(user):
+    response = chat.send_message(user)
+    return response
+
 
 # React to user input
 if prompt := st.chat_input("What is up?"):
@@ -19,8 +32,8 @@ if prompt := st.chat_input("What is up?"):
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-
-    response = f"Echo: {prompt}"
+    bot = get_response(promt)
+    response = f"{bot}"
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st.markdown(response)
